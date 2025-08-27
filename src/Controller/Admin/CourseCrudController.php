25b80b99b7859010a7b313de73b13eb41c28a391
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-08-27 10:12:30 +0200

    list-course #13 : create CourseCrudController and customize

---
+++ src/Controller/Admin/CourseCrudController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Course;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+/**
+ * @extends AbstractCrudController<Course>
+ */
+class CourseCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Course::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDefaultSort(['id' => 'DESC'])
+            ->setPaginatorPageSize(10)
+            ->setSearchFields(['name'])
+            ->setEntityLabelInPlural('cours')
+            ->setEntityLabelInSingular('cour')
+            ->setPageTitle('index', 'Liste des %entity_label_plural%')
+            ->showEntityActionsInlined()
+        ;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name', 'Nom du cour'),
+            DateField::new('createdAt', 'Date de publication'),
+            BooleanField::new('published', 'Publié ?'),
+            AssociationField::new('category', 'Catégorie'),
+        ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+                return $action->setLabel('Ajouter');
+            })
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('name')
+            ->add('published')
+            ->add('category')
+            ->add('createdAt')
+        ;
+    }
+}
