3107a0d7c340f61ab46ae410d06e4281dc13797d
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-09-01 16:12:34 +0200

    course #14 : Creating the page that displays course information

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Course;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class CourseController extends AbstractController
@@ -11,8 +13,14 @@ final class CourseController extends AbstractController
     #[Route('/cours', name: 'course.list', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('course/index.html.twig', [
-            'controller_name' => 'CourseController',
+        return $this->render('course/index.html.twig');
+    }
+
+    #[Route('/cours/{slug}', name: 'course.show', methods: ['GET'])]
+    public function show(#[MapEntity(mapping: ['slug' => 'slug'])] Course $course): Response
+    {
+        return $this->render('course/show.html.twig', [
+            'course' => $course,
         ]);
     }
 }
