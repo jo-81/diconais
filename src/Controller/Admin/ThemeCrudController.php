3aad40e610638ce5ac6bf2fc5dbc5335f56fb79f
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-09-17 08:43:28 +0200

    list-theme #31 : Creating the Theme entity and the CrudController Theme to display the list of themes

---
+++ src/Controller/Admin/ThemeCrudController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Theme;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ThemeCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Theme::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDefaultSort(['id' => 'DESC'])
+            ->setPaginatorPageSize(10)
+            ->setSearchFields(['name'])
+            ->setEntityLabelInPlural('thèmes')
+            ->setEntityLabelInSingular('thème')
+            ->setPageTitle('index', 'Liste des %entity_label_plural%')
+            ->showEntityActionsInlined()
+        ;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'Theme'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            TextField::new('description'),
+        ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+                return $action->setLabel('Ajouter');
+            })
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters->add('name');
+    }
+}
---
+++ src/Entity/Theme.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ThemeRepository;
+
+#[ORM\Entity(repositoryClass: ThemeRepository::class)]
+class Theme
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
