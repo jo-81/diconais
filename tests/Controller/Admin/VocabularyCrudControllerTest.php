154710282878f0d4847fa1e941331159e69366e7
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-09-24 09:49:35 +0200

    add-vocabulary #24 : add a vocabulary entity with tests

--- tests/Controller/Admin/VocabularyCrudControllerTest.php
+++ tests/Controller/Admin/VocabularyCrudControllerTest.php
@@ -3,12 +3,21 @@
 namespace App\Tests\Controller\Admin;
 
 use App\Entity\User;
+use App\Entity\Vocabulary;
+use App\Tests\Traits\EntityFinderTrait;
 use App\Controller\Admin\DashboardController;
 use App\Controller\Admin\VocabularyCrudController;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 use EasyCorp\Bundle\EasyAdminBundle\Test\AbstractCrudTestCase;
 
 class VocabularyCrudControllerTest extends AbstractCrudTestCase
 {
+    /**
+     * @use EntityFinderTrait<Vocabulary>
+     */
+    use EntityFinderTrait;
+    use ReloadDatabaseTrait;
+
     protected function getControllerFqcn(): string
     {
         return VocabularyCrudController::class;
@@ -29,6 +38,9 @@ class VocabularyCrudControllerTest extends AbstractCrudTestCase
 
         $this->client->request('GET', $this->generateDetailUrl(1));
         static::assertResponseRedirects('/connexion');
+
+        $this->client->request('GET', $this->generateNewFormUrl());
+        static::assertResponseRedirects('/connexion');
     }
 
     /**
@@ -45,5 +57,28 @@ class VocabularyCrudControllerTest extends AbstractCrudTestCase
 
         $this->client->request('GET', $this->generateDetailUrl(1));
         static::assertResponseIsSuccessful();
+
+        $this->client->request('GET', $this->generateNewFormUrl());
+        static::assertResponseIsSuccessful();
+    }
+
+    public function testCreateEntityKanji(): void
+    {
+        $testUser = $this->findOneEntityBy(User::class, ['email' => 'admin@domaine.fr']);
+        $this->client->loginUser($testUser);
+        $crawler = $this->client->request('GET', $this->generateNewFormUrl());
+
+        $form = $crawler->selectButton('Créer')->form();
+        $form['Vocabulary[signification]'] = 'vocabulary';
+        $form['Vocabulary[reading]'] = 'reading';
+        $form['Vocabulary[romaji]'] = 'romaji';
+        $this->client->submit($form);
+
+        self::assertInstanceOf(Vocabulary::class, $this->findOneEntityBy(Vocabulary::class, ['signification' => 'vocabulary']));
+        self::assertResponseRedirects();
+
+        $this->client->followRedirect();
+
+        $this->assertSelectorTextContains('div', "'vocabulary' a été créé avec succès.");
     }
 }
