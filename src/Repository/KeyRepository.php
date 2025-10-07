75cf736d7cc6020f555d6110a76407f9e0dd9cba
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-10-07 08:21:56 +0200

    refactoring #80 : Ajout constante ALLOWED_OPERATORS pour Kanji et KeyRepository

--- src/Repository/KeyRepository.php
+++ src/Repository/KeyRepository.php
@@ -11,6 +11,8 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  */
 class KeyRepository extends ServiceEntityRepository
 {
+    private const ALLOWED_OPERATORS = ['=', '>', '<', '>=', '<='];
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Key::class);
@@ -53,39 +55,14 @@ class KeyRepository extends ServiceEntityRepository
         }
 
         if (isset($strokes['numberStroke']) && !is_null($strokes['numberStroke'])) {
-            $egal = $strokes['egal'] ?: '=';
+            $operator = in_array($strokes['egal'] ?? '=', self::ALLOWED_OPERATORS)
+                ? $strokes['egal']
+                : '=';
 
-            $qb
-                ->andWhere("k.numberStroke $egal :numberStroke")
-                ->setParameter('numberStroke', $strokes['numberStroke'])
-            ;
+            $qb->andWhere("k.numberStroke {$operator} :numberStroke")
+                ->setParameter('numberStroke', $strokes['numberStroke']);
         }
 
         return $qb->getQuery()->getResult();
     }
-
-    //    /**
-    //     * @return Key[] Returns an array of Key objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('k')
-    //            ->andWhere('k.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('k.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Key
-    //    {
-    //        return $this->createQueryBuilder('k')
-    //            ->andWhere('k.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
